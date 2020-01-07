92c9c3db30a9355ddc62e8a80ea3c281c261fccc
Diane Peshlakai <dpeshlakai90@gmail.com>
2020-01-07 14:52:55 -0700

    added name

---
+++ public_html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<title>PHP project-testing</title>
+</head>
+<body>
+<?php
+     echo 'hello Diane ';
+     //testing testing
+?>
+</body>
+</html>

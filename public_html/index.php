c44ea72437d71d6fc1186b9ec8781b47e9013b80
Diane Peshlakai <dpeshlakai90@gmail.com>
2020-01-24 14:56:08 -0700

    adding bootstrap

--- public_html/index.php
+++ public_html/index.php
@@ -2,13 +2,27 @@
 <html lang="en-US">
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
 <title>PHP project-testing</title>
+<!-- Font Awesome Icons -->
+ <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+ <!-- Google Fonts -->
+
+ <!-- Plugin CSS -->
+ <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+ <!-- Theme CSS - Includes Bootstrap -->
+ <link href="css/creative.min.css" rel="stylesheet">
 </head>
 <body>
 <?php
      echo 'hello Diane';
      //testing testing
-     //testing 
+     //testing
 ?>
 </body>
 </html>

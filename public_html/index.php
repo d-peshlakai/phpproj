55a5ba348c3dff88188f78ac15d6a977ac4f4ea3
Diane Peshlakai <dpeshlakai90@gmail.com>
2020-04-13 12:08:02 -0600

    enter title for section

--- public_html/index.php
+++ public_html/index.php
@@ -64,7 +64,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 mx-auto">
-          <h2 class="text-white mb-4">Login Page</h2>
+          <h2 class="text-white mb-4">Diane Login Page</h2>
           <p class="text-white-50">
             <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
         </div>

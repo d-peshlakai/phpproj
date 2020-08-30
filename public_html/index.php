48174b3552173398d887989751bba641232353d1
Diane Peshlakai <dpeshlakai90@gmail.com>
2020-08-29 19:53:41 -0600

    fixed tags link

--- public_html/index.php
+++ public_html/index.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html lang="en-US">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+  <meta name="description" content=""/>
+  <meta name="author" content=""/>
 
 <title>PHP project-testing</title>
 
 <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
 <!-- Font Awesome Icons -->
- <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+ <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"/>
 
  <!-- Google Fonts -->
 
  <!-- Plugin CSS -->
- <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+ <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet"/>
 
  <!-- Theme CSS - Includes Bootstrap -->
- <link href="websites/css/stylesheet.css" rel="stylesheet">
+ <link href="websites/css/stylesheet.css" rel="stylesheet"/>
 </head>
 <body id="top-page">
 
@@ -93,7 +93,6 @@
 
 <!-- Bootstrap core JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
